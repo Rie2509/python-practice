e4a9e74cbc37804b421ecad0301b03874e23a174
Rie2509 <159850198+Rie2509@users.noreply.github.com>
2024-05-02 12:24:38 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Ask the user their name and save it
+name = input ("what is your name")
+print ("welcome to the quiz",name)
+#Greet the user and introduce the quiz
+print ("this is a quiz on your valorant")
+#Ask the user a question
+input ("what is the most expensive weapon in valorant")
+#Tell them the correct answer
+print ("The operator")
+print ("the end")
